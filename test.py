3c917966e135fbcd9bc2d4f96e022e04f378b59a
dannykhant <dannypmkhant@gmail.com>
2026-01-14 09:55:02 +0700

    feat: test script for server

---
+++ test.py
@@ -0,0 +1,16 @@
+import requests
+
+url = 'http://localhost:8080/generate'
+
+request = {
+    "prompt": "Once upon a time, lily found",
+    "max_tokens": 30
+}
+
+result = requests.post(url, json=request)
+
+elapsed_time = result.elapsed
+response_time_seconds = elapsed_time.total_seconds()
+
+print(f"Response time: {response_time_seconds} seconds")
+print(f"Result: {result.json()}")
